a205ec28cad2b26b738e63f73a2b9b59895c47f8
DanielBarrera-a <daniel.barrera-a@mail.escuelaing.edu.co>
2026-08-27 17:11:28 -0500

    Aplicattio areglado

---
+++ src/main/java/edu/dosw/lab/Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab;
+
+import edu.dosw.lab.SOLID.reto1.Reto1BoleteriaAstor;
+import edu.dosw.lab.estructurales.reto4.Reto4BalanzaTrucada;
+import edu.dosw.lab.estructurales.reto5.Reto5MotoPersonalizada;
+
+public class Application {
+    public static void main(String[] args) {
+        Reto1BoleteriaAstor.ejecutar();
+        Reto4BalanzaTrucada.ejecutar();
+        Reto5MotoPersonalizada.ejecutar();
+    }
+}
